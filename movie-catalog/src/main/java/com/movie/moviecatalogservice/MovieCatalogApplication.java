d142b2fe5d014ba7e6da74fd79c3effd184b41c9
suhas <suhascomp@gmail.com>
2021-04-21 11:32:59 +0530

    added netflix hystrix dependency

--- movie-catalog/src/main/java/com/movie/moviecatalogservice/MovieCatalogApplication.java
+++ movie-catalog/src/main/java/com/movie/moviecatalogservice/MovieCatalogApplication.java
@@ -2,6 +2,7 @@ package com.movie.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +12,15 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogApplication {
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate getTemplate() {
-		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-		httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);
-		return new RestTemplate(httpComponentsClientHttpRequestFactory);
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 //	@Bean
