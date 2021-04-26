8c77782acd88fe21b3607b42d2ca2696622d4c48
suhas <suhascomp@gmail.com>
2021-04-26 21:14:11 +0530

    Property file and @Value annotation common usage

---
+++ movie-catalog/src/main/java/com/movie/moviecatalogservice/resource/GreetController.java
@@ -0,0 +1,40 @@
+package com.movie.moviecatalogservice.resource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetController {
+
+    @Value("${my.greetings}")
+    private String greetings;
+
+    @Value("${my.default: default value }")
+    private String defaultVal;
+
+    @Value(" Static string")
+    private String staticString;
+
+    @Value("${my.list.numbers}")
+    private List<String> numbers;
+
+    @Value("#{${my.map.dbValues}}")
+    private Map<String, String> dbDetails;
+
+    @GetMapping("/welcome")
+    public String greetings() {
+        return greetings + defaultVal + staticString;
+    }
+
+    @GetMapping("/details")
+    public String getDetails() {
+        return " " + numbers + dbDetails;
+    }
+
+}
